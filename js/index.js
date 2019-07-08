b0d881885043b568331b3df324a1eb3ee4147168
纽特 <coorchice.cb@alibaba-inc.com>
2019-07-08 18:07:40 +0800

    update

---
+++ js/index.js
@@ -0,0 +1,25 @@
+function test() {
+    $.get("https://raw.githubusercontent.com/chenBingX/CoorChiceArticale/master/SuperTextView%E6%96%87%E6%A1%A3/Github%E7%89%88%E6%9C%AC-%E4%B8%AD%E6%96%87.md", function(data, status) {
+        if (status == "success") {
+            var md = data.responseText;
+            let html_content = markdown.toHTML(data);
+            console.log(html_content);
+            let html = "<!DOCTYPE html>" +
+                "<html>" +
+                "<head>" +
+                "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/html.css\"" +
+                // "<script type=\"text/javascript\" src=\"js/markdown.js\"></script>" +
+                // "<script>hljs.initHighlightingOnLoad();</script>" +
+                "</head>" +
+                "<body>" +
+                html_content +
+                "</body>" +
+                "</html>";
+            document.write(html);
+        }
+        // var md = data.responseText;
+        // console.log("status = " + status);
+        // console.log(data);
+        // console.log(markdown.toHTML(md));
+    });
+}
