73b8858aca4dea213e98dae80d25b8d4cd3f4da6
纽特 <coorchice.cb@alibaba-inc.com>
2019-07-11 16:42:36 +0800

    update

--- js/articlelist.js
+++ js/articlelist.js
@@ -1,27 +1,36 @@
 var token = "token e2d4e6c541bb41efcd1a9e6e8e06dd59a46138f2"
 
-function getArticles(url) {
+function getArticles(url, spaceCount) {
     $.ajax({
         type: "GET",
         url: url,
         cache: true,
-        async: true,
+        async: false,
         beforeSend: function(request) {
             // request.setRequestHeader("Authorization", token);
         },
         success: function(data, status, xhr) {
             if (status == "success" && data != null && data instanceof Array) {
                 // console.log("data = " + JSON.stringify(data));
+                let space = "";
+                for (let i = 0; i < spaceCount; i++) {
+                    // space += "&nbsp;";
+                    space += "--";
+                }
+                console.log("space = " + space + "!");
                 data.forEach(element => {
                     if (element.type == "file" && element.name.indexOf(".md") != -1 && element.name != "README.md") {
                         // console.log("article = " + JSON.stringify(element));
                         let article_name = element.name.replace(".md", "");
-                        let a_str = "<a href='article.html?article_url=" + element.download_url + "' target='_blank'>" + article_name + "</a>";
+                        let a_str = "<a href='article.html?article_url=" + element.download_url + "' target='_blank'>" + space + "- " + article_name + "</a>";
                         let a = $(a_str);
-                        $("section.content").append(a).append($("<br>"));
+                        $("section.content").append(a);
                     } else if (element.type == "dir" && element.name.indexOf(".") == -1) {
                         ("-------------------这是一个文件夹!-------------------");
-                        getArticles(element.url);
+                        let a_str = "<a class='title' href='javascript:void(0)'>" + space + element.name + "</a>";
+                        let a = $(a_str);
+                        $("section.content").append(a);
+                        getArticles(element.url, spaceCount + 1);
                     }
                 });
             }
