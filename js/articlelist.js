94883ed38cd709e87d7144b25cc671681a3a68b4
纽特 <coorchice.cb@alibaba-inc.com>
2019-07-11 19:40:20 +0800

    update

--- js/articlelist.js
+++ js/articlelist.js
@@ -1,5 +1,3 @@
-var token = "token e2d4e6c541bb41efcd1a9e6e8e06dd59a46138f2"
-
 function getArticles(url, spaceCount) {
     $.ajax({
         type: "GET",
@@ -14,26 +12,25 @@ function getArticles(url, spaceCount) {
                 // console.log("data = " + JSON.stringify(data));
                 let space = "";
                 for (let i = 0; i < spaceCount; i++) {
-                    // space += "&nbsp;";
-                    space += "--";
+                    space += "&nbsp;&nbsp;";
+                    // space += "--";
                 }
                 console.log("space = " + space + "!");
                 data.forEach(element => {
                     if (element.type == "file" && element.name.indexOf(".md") != -1 && element.name != "README.md") {
                         // console.log("article = " + JSON.stringify(element));
                         let article_name = element.name.replace(".md", "");
-                        let a_str = "<a href='article.html?article_url=" + element.download_url + "' target='_blank'>" + space + " " + article_name + "</a>";
-                        let a = $(a_str);
-                        $("section.content").append(a);
+                        let a_str = "<a href='article.html?article_url=" + element.download_url + "' target='_blank'>" + space + "- " + article_name + "</a>";
+                        let a = $("<li>" + a_str + "</li>");
+                        $("#list").append(a);
                     } else if (element.type == "dir" && element.name.indexOf(".") == -1) {
-                        ("-------------------这是一个文件夹!-------------------");
                         let titleSpace = space;
-                        if (titleSpace.indexOf("-") != 1) {
-                            titleSpace = titleSpace.replace(/-/g, "+");
-                        }
-                        let a_str = "<a class='title' href='javascript:void(0)'>" + titleSpace + " " + element.name + "</a>";
-                        let a = $(a_str);
-                        $("section.content").append(a);
+                        // if (titleSpace.indexOf("-") != 1) {
+                        //     titleSpace = titleSpace.replace(/-/g, "+");
+                        // }
+                        let a_str = "<a class='title' href='javascript:void(0)'>" + titleSpace + element.name + "</a>";
+                        let a = $("<li>" + a_str + "</li>");
+                        $("#list").append(a);
                         getArticles(element.url, spaceCount + 1);
                     }
                 });
