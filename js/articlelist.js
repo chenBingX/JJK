d9e25f4f83f94cf7b639a7846353fc9e7fed5cd5
纽特 <coorchice.cb@alibaba-inc.com>
2019-07-11 14:48:20 +0800

    update

--- js/articlelist.js
+++ js/articlelist.js
@@ -1,27 +1,45 @@
-function getArticles() {
-    let url = "https://api.github.com/repos/chenBingX/CoorChiceArticale/contents";
-    $.get(url, function(data, status) {
-        if (status == "success" && data != null) {
-            getArticles2(data[6].url);
+function getArticles(url) {
+    $.ajax({
+        type: "GET",
+        url: url,
+        cache: true,
+        beforeSend: function(request) {
+            request.setRequestHeader("Authorization", "token 3592734248df224971b6690fe6431add200e181b");
+        },
+        success: function(data, status, xhr) {
+            if (status == "success" && data != null) {
+                data.forEach(element => {
+                    getSubarticles(element.url);
+                });
+            }
         }
     });
 }
 
-function getArticles2(url) {
-    $.get(url, function(data, status) {
-        if (status == "success" && data != null) {
-            console.log("data = " + JSON.stringify(data));
-            data.forEach(element => {
-                if (element.type == "file") {
-                    console.log("article = " + JSON.stringify(element));
-                    let a_str = "<a href='article.html?article_url=" + element.download_url + "'>" + element.name + "</a>";
-                    console.log(a_str);
-                    let a = $(a_str);
-                    $("section.content").append(a).append($("<br>"));
-                } else if (element.type == "dir") {
-                    console.log("文件夹");
-                }
-            });
+function getSubarticles(url) {
+    $.ajax({
+        type: "GET",
+        url: url,
+        cache: true,
+        beforeSend: function(request) {
+            request.setRequestHeader("Authorization", "token 3592734248df224971b6690fe6431add200e181b");
+        },
+        success: function(data, status, xhr) {
+            if (status == "success" && data != null) {
+                console.log("data = " + JSON.stringify(data));
+                data.forEach(element => {
+                    if (element.type == "file") {
+                        console.log("article = " + JSON.stringify(element));
+                        let a_str = "<a href='article.html?article_url=" + element.download_url + "' target='_blank'>" + element.name + "</a>";
+                        console.log(a_str);
+                        let a = $(a_str);
+                        $("section.content").append(a).append($("<br>"));
+                    } else if (element.type == "dir") {
+                        console.log("文件夹");
+                        getArticles(element.url);
+                    }
+                });
+            }
         }
     });
 }
